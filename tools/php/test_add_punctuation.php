030018aa1d29735cf63dd3b9c17090d681fa2097
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-04-11 18:16:30 -0500

    Updated

---
+++ tools/php/test_add_punctuation.php
@@ -0,0 +1,101 @@
+<?php
+/*
+php H:\github\CanonicalTextTrees\tools\php\test_add_punctuation.php
+ */
+
+require_once( 
+	dirname( __DIR__, 3 ) . DIRECTORY_SEPARATOR .
+	'Dufu-Analysis' . DIRECTORY_SEPARATOR .
+	'tools' . DIRECTORY_SEPARATOR .
+	'php' . DIRECTORY_SEPARATOR .
+	'lib' . DIRECTORY_SEPARATOR .
+	 '函式.php' );
+
+require_once( 
+	__DIR__ . DIRECTORY_SEPARATOR .
+	 'functions.php' );
+
+$path = dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR .
+	'論語' . DIRECTORY_SEPARATOR .
+	'trees' . DIRECTORY_SEPARATOR .
+	'02.json';
+
+$tree = json_decode( 
+	file_get_contents( $path ), true );
+	
+foreach( $tree as $k => $v )
+{
+	if( is_string( $v ) )
+	{
+		$tree[ $k ] = '《' . $v . '》';
+		continue;
+	}
+	else
+	{
+		add_punctuation( $tree[ $k ] );
+	}
+}
+
+$contents = [];
+
+foreach( $tree as $k => $v )
+{
+	if( is_string( $v ) )
+	{
+		$contents[] = $tree[ $k ];
+	}
+	else
+	{
+		$contents[] = 
+			flatten_tree_to_text_skip_keys( $tree[ $k ] ) .
+			NL;
+	}
+}
+
+$path = dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR .
+	'論語' . DIRECTORY_SEPARATOR .
+	'views' . DIRECTORY_SEPARATOR .
+	'02.md';
+
+$txt = '';
+
+for( $i = 0; $i < count( $contents ); $i++ )
+{
+	if( $i == 0 )
+	{
+		$txt .= '# ' . $contents[ $i ] . NL . NL;
+		$txt .= "此文檔以 PHP 生成。" . NL . NL;
+	}
+	else
+	{
+		$txt .= '## ' . $i . NL . NL;
+		$txt .= $contents[ $i ] . NL . NL;
+	}
+}
+
+file_put_contents( $path, $txt );
+
+//print_r( $contents );
+
+function add_punctuation( 
+	array &$tree, string $punc='。' ) : void
+{
+	$keys = array_keys( $tree );
+	$values = array_values( $tree );
+	
+	if( is_string( $values[ 0 ] ) )
+	{
+		$pointer = &$tree[ count( $keys ) ];
+		$pointer = $pointer . $punc;
+		return;
+	}
+	
+	elseif( is_array( $values[ 0 ] ) )
+	{
+		foreach( $tree as $key => $value )
+		{
+			add_punctuation( $tree[ $key ], $punc );
+		}
+	}
+}
+?>
