522dae0785ce9c7b31af07810c51e3f431f5d53b
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-04-24 11:26:18 -0500

    Updated

--- tools/php/清除李善注.php
+++ tools/php/清除李善注.php
@@ -18,14 +18,31 @@ require_once(
 
 $work_id = 'WENXUAN';
 $folder = get_folder( $work_id );
-
-$filename = '26.31';
-$filepath = dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR .
+$raw_dir = dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR .
 	$folder . DIRECTORY_SEPARATOR .
-	'raw_text' . DIRECTORY_SEPARATOR .
-	$filename . '.txt';
-$ptn = '/〈\X+?〉/u';
-$contents = file_get_contents( $filepath );
-$contents = preg_replace( $ptn, '', $contents );
-file_put_contents( $filepath, $contents );
+	'raw_text' . DIRECTORY_SEPARATOR;
+
+if( !is_dir( $raw_dir ) )
+{
+    throw new RuntimeException(
+		'文件夾不存在: ' . $raw_dir );
+}
+$files = scandir( $raw_dir );
+sort( $files, SORT_STRING );
+
+foreach( $files as $file )
+{
+	$path = $raw_dir . $file;
+
+	if(
+		is_file( $path )
+		&& preg_match( '/\.txt$/i', $file )
+	)
+	{
+		$ptn = '/〈\X+?〉/u';
+		$contents = file_get_contents( $path );
+		$contents = preg_replace( $ptn, '', $contents );
+		file_put_contents( $path, $contents );
+	}
+}
 ?>
