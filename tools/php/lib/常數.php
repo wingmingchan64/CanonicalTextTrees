2e55d12e6464d557153f23dd2abb2225740a4a18
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-04-28 10:52:04 -0500

    Updated

--- tools/php/lib/常數.php
+++ tools/php/lib/常數.php
@@ -1,8 +1,4 @@
 <?php
-const FOLDER        = 'folder';
-const TITLE         = 'title';
-const DISPLAY_TITLE = 'display_title';
-const NUM_OF_CHAPTERS = 'num_of_chapters';
 const 作者 = '作者';
 const 詞牌 = '詞牌';
 const 篇名 = '篇名';
