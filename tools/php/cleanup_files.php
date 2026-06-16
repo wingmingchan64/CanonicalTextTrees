33189b5d00c0ea3b8c39abe7b83b531ccecf4e38
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-06-16 12:15:22 -0500

    Updated

--- tools/php/cleanup_files.php
+++ tools/php/cleanup_files.php
@@ -24,28 +24,35 @@ $target_folder = dirname( __DIR__, 2 ) .
 	'canonical_text' . DIRECTORY_SEPARATOR;
 	//'raw_text' . DIRECTORY_SEPARATOR;
 */
-$target_folder = "H:\github\DuFu\默認版本\文賦\\";
-
+//$target_folder = "H:\github\DuFu\默認版本\文賦\\";
+/*
 if( !is_dir( $target_folder ) )
 {
     throw new RuntimeException( '文件夾不存在: ' . $target_folder );
 }
 $files = scandir( $target_folder );
 sort( $files, SORT_STRING );
-
-foreach( $files as $file )
-{
-	$path = $target_folder . $file;
+*/
+//foreach( $files as $file )
+//{
+	//$path = $target_folder . $file;
+	$path = "H:\github\CanonicalTextTrees\corpus\dufu\杜詩詳註\canonical_text" . '\0145.txt';
 
 	if(
 		is_file( $path )
-		&& preg_match( '/\.txt$/i', $file )
+		//&& preg_match( '/\.txt$/i', $file )
 	)
 	{
+		echo "File", NL;
 		$txt = file_get_contents( $path );
-		$txt = normalize( 修復文字( $txt ) );
+		//$txt = normalize( 修復文字( $txt ) );
+		$txt = 修復文字( $txt );
 		//echo $txt, NL;
 		file_put_contents( $path, $txt );
 	}
-}
+	else
+	{
+		echo "not found";
+	}
+//}
 ?>
