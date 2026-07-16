11a6ab4fa1ffb0a9483d8984aae02dfe71ffba77
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-07-16 14:58:48 -0500

    Updated

--- tools/php/bin/執行路徑程式.php
+++ tools/php/bin/執行路徑程式.php
@@ -15,13 +15,13 @@ require_once(
 	 'functions.php' );
 	 
 //$work_id = 'CHOUZHU';
-//$work_id = 'DFQJYYZY';
+$work_id = 'DFQJYYZY';
 //$work_id = 'DFQSYZ';
 //$work_id = 'DFSQY';
 //$work_id = 'GUOZHU';
 //$work_id = 'JINGQUAN';
 //$work_id = 'OWEN';
-$work_id = 'TSJSCD';
+//$work_id = 'TSJSCD';
 //$work_id = 'XDF';
 //$work_id = 'ZDZSQTS';
 //$work_id = 'ZHANGNIANPU';
