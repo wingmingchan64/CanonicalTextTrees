bf468566a469d23bf2394d074afd4e40155e387d
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-08-09 10:53:57 -0500

    Updated

---
+++ tools/php/bin/bach_cantatas/生成德語正文樹、詞典.php
@@ -0,0 +1,117 @@
+<?php
+/*
+php H:\github\CanonicalTextTrees\tools\php\bin\bach_cantatas\生成德語正文樹、詞典.php
+ */
+
+require_once( 
+	dirname( __DIR__, 5 ) . DIRECTORY_SEPARATOR .
+	'Dufu-Analysis' . DIRECTORY_SEPARATOR .
+	'tools' . DIRECTORY_SEPARATOR .
+	'php' . DIRECTORY_SEPARATOR .
+	'lib' . DIRECTORY_SEPARATOR .
+	 '函式.php' );
+require_once( 
+	dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR .
+	'lib' . DIRECTORY_SEPARATOR .
+	 'functions.php' );
+	 
+$著述碼 = 'BACH_CANTATAS';
+$folder = dirname( __DIR__, 4 ) . DIRECTORY_SEPARATOR .
+	get_ctt_folder( $著述碼 ) . DIRECTORY_SEPARATOR;
+$trees_folder = $folder . 'trees' . DIRECTORY_SEPARATOR;
+$views_folder = $folder . 'views' . DIRECTORY_SEPARATOR;
+	
+$bwv = '140';
+$filename = $bwv . '_DE';
+$file = $trees_folder . $filename . '.json';
+	
+if( file_exists( $file ) )
+{
+	$tree = 
+		json_decode(
+			file_get_contents( $file ), true );
+}
+
+$樹 = array( $bwv => array() );
+
+foreach( $tree[ $filename ] as $k => $v )
+{
+	if( $k == '篇名' )
+	{
+		$樹[ $bwv ][ '篇名' ] = $tree[ $filename ][ '篇名' ];
+	}
+	else
+	{
+		foreach( array_keys( $v ) as $line )
+		{
+			$樹[ $bwv ][ $k ][ $line ][ 'text' ] = 
+				$tree[ $filename ][ $k ][ $line ];
+		}
+	}
+}
+
+$metadata = json_decode( 
+	file_get_contents( $folder . 'metadata' . 
+	DIRECTORY_SEPARATOR . $bwv . '.json' ), true );
+
+$german_folder = dirname( __DIR__, 4 ) .
+	DIRECTORY_SEPARATOR .
+	get_ctt_folder( 'GERMAN' ) . DIRECTORY_SEPARATOR;
+$pronunciation_file = $german_folder . 'pronunciation.json';
+$pronunciation = json_decode(
+	file_get_contents( $pronunciation_file ), true);
+$grammar_file = $german_folder . 'grammar.json';
+$grammar = json_decode(
+	file_get_contents( $grammar_file ), true);
+$Langenscheidt_file = $german_folder . 'Langenscheidt.json';
+$Langenscheidt = json_decode(
+	file_get_contents( $Langenscheidt_file ), true);
+$Cambridge_file = $german_folder . 'Cambridge.json';
+$Cambridge = json_decode(
+	file_get_contents( $Cambridge_file ), true);
+//print_r( $樹 );
+
+foreach( $metadata as $path => $term )
+{
+	$paths = explode( ',', $path );
+	$pointer = &$樹;
+	
+	foreach( $paths as $step )
+	{
+		$pointer = &$pointer[ $step ];
+	}
+	if( !array_key_exists( 'entry', $pointer ) )
+	{
+		$pointer[ 'entry' ] = array();
+	}
+	if( !array_key_exists( $term, $pointer[ 'entry' ] ) )
+	{
+		$pointer[ 'entry' ][ $term ] = array();
+	}
+	if( array_key_exists( $term, $pronunciation ) )
+	{
+		$pointer[ 'entry' ][ $term ][ 'pronunciation' ]
+			= $pronunciation[ $term ];
+	}
+	if( array_key_exists( $term, $grammar ) )
+	{
+		$pointer[ 'entry' ][ $term ][ 'grammar' ]
+			= $grammar[ $term ];
+	}
+	if( array_key_exists( $term, $Langenscheidt ) )
+	{
+		$pointer[ 'entry' ][ $term ][ 'Langenscheidt' ]
+			= $Langenscheidt[ $term ];
+	}
+	if( array_key_exists( $term, $Cambridge ) )
+	{
+		$pointer[ 'entry' ][ $term ][ 'Cambridge' ]
+			= $Cambridge[ $term ];
+	}
+}
+
+file_put_contents(
+	$views_folder . $bwv . '_dic.json',
+	json_encode(
+		$樹, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) );
+?>
