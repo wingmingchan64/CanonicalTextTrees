71ecd87e5998f6d57777c326f08b8bf5908b62e6
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-09-05 16:18:21 -0500

    Updated

--- tools/php/bin/views/版本文檔碼→詩文.php
+++ tools/php/bin/views/版本文檔碼→詩文.php
@@ -20,18 +20,24 @@ $paths = array();
 記錄後設資料樹路徑( $mm_tree );
 添加標點符號( $正文樹 );
 
+// replace 詩題
+$篇名path = $著述碼 . ',' . $版文檔碼 . ',' . '篇名';
+$正文樹[ 詩題 ] = 提取ctt正文( $篇名path );
+
 foreach( $paths as $path )
 {
 	$parts = explode( '_', $path );
 	$默路徑 = explode( ',', $parts[ 3 ] );
 	$異文 = 提取ctt正文( $parts[ 4 ] );
+	//echo $異文 . ":" . $parts[ 4 ], NL;
 	$op = $parts[ 5 ];
 	$pointer = &$正文樹;
-	
+		
 	foreach( $默路徑 as $step )
 	{
 		$pointer = &$pointer[ $step ];
 	}
+	
 	if( $op == 'replace' )
 	{
 		$pointer = $異文;
@@ -44,4 +50,5 @@ foreach( $paths as $path )
 
 echo $正文樹[ $默文檔碼 ][ 詩題 ], NL, NL;
 echo 攤平樹文字_略過鍵( $正文樹, array( 詩題 ) );
+
 ?>
